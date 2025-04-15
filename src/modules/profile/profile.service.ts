6eb75f4a741044477b64db70f5a4b3c47a0f4284
EseToni <Falsigfx@gmail.com>
2025-04-15 18:57:13 +0200

    refactor(profile): comment out profile creation logic and return profile data instead

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -14,35 +14,35 @@ export class ProfileService {
     const profileData =
       await this.profileExtractionApiService.getProfile(publicId);
 
-    // Save the extracted profile data to the database using ProfileDbService
-    const createdProfile = await this.profileDbService.create({
-      ...profileData,
-      publicId: publicId,
-      user: {
-        connect: {
-          id: userId,
-        },
-      },
-      experiences: {
-        create: profileData.experience.map((experience) => ({
-          ...experience,
-          startDate: experience.timePeriod.startDate
-            ? new Date(experience.timePeriod.startDate.month)
-            : null,
-          endDate: experience.timePeriod.endDate
-            ? new Date(experience.timePeriod.endDate.month)
-            : null,
-          company: {
-            create: {
-              name: experience.companyName,
-              employeeCountRange: experience.company?.employeeCountRange || null,
-              industries: experience.company?.industries || [],
-            },
-          },
-        })),
-      },
-    });
+    // // Save the extracted profile data to the database using ProfileDbService
+    // const createdProfile = await this.profileDbService.create({
+    //   ...profileData,
+    //   publicId: publicId,
+    //   user: {
+    //     connect: {
+    //       id: userId,
+    //     },
+    //   },
+    //   experiences: {
+    //     create: profileData.experience.map((experience) => ({
+    //       ...experience,
+    //       startDate: experience.timePeriod.startDate
+    //         ? new Date(experience.timePeriod.startDate.month)
+    //         : null,
+    //       endDate: experience.timePeriod.endDate
+    //         ? new Date(experience.timePeriod.endDate.month)
+    //         : null,
+    //       company: {
+    //         create: {
+    //           name: experience.companyName,
+    //           employeeCountRange: experience.company?.employeeCountRange || null,
+    //           industries: experience.company?.industries || [],
+    //         },
+    //       },
+    //     })),
+    //   },
+    // });
 
-    return createdProfile;
+    return profileData;
   }
 }
