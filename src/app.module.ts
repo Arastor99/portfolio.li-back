ed1cdeaa5a699c27db031db271c2e1ff25f2596c
EseToni <Falsigfx@gmail.com>
2025-06-14 12:26:01 +0200

    feat(activity): reorganize imports and ensure ActivityDbModule is included in ActivityModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './modules/auth/auth.module';
-import { PrismaModule } from './models/prisma.module';
+
 import { envValidationSchema } from './config/env.validation';
+
+import { PrismaModule } from './models/prisma.module';
+
+import { AuthModule } from './modules/auth/auth.module';
 import { ProfileModule } from './modules/profile/profile.module';
 import { PortfolioTemplateModule } from './modules/portfolio-template/portfolio-template.module';
 import { ProxyModule } from './modules/proxy/proxy.module';
 import { PortfolioModule } from './modules/portfolio/portfolio.module';
 import { CvModule } from './modules/cv/cv.module';
 import { CvTemplateModule } from './modules/cv-template/cv-template.module';
+import { ActivityModule } from './modules/activity/activity.module';
 
 @Module({
   imports: [
@@ -24,6 +28,7 @@ import { CvTemplateModule } from './modules/cv-template/cv-template.module';
     ProxyModule,
     CvModule,
     CvTemplateModule,
+    ActivityModule,
   ],
 })
 export class AppModule {}
--- src/modules/activity/activity.module.ts
+++ src/modules/activity/activity.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ActivityService } from './activity.service';
 import { ActivityController } from './activity.controller';
+import { ActivityDbModule } from 'src/models/activity/activity.db.module';
 
 @Module({
+  imports: [ActivityDbModule],
   controllers: [ActivityController],
   providers: [ActivityService],
 })
